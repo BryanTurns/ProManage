ef6456f0b615fb21baec632e4e64719c6e99ed5d
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2024-04-04 11:10:06 -0600

    Fixed issue with middlewear denying access to some pages for unit testing

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -60,10 +60,14 @@ app.use(
 const auth = (req, res, next) => {
   if (
     !req.session.user &&
-    req.url != "/login" &&
-    req.url != "/register" &&
-    req.url != "/register_manager" &&
-    req.url != "/register_employee"
+    // req.url != "/login" &&
+    // req.url != "/register" &&
+    // req.url != "/register_manager" &&
+    // req.url != "/register_employee"
+    (req.url == "/tasks" ||
+      req.url == "/logout" ||
+      req.url == "/" ||
+      req.url == "/home")
   ) {
     // Default to login page.
     return res.redirect("/login");
@@ -77,7 +81,7 @@ app.get("/", (req, res) => {
   res.render("./pages/home", { auth: req.session.user });
 });
 app.get("/home", (req, res) => {
-  res.redirect("/", {});
+  res.redirect("/");
 });
 app.get("/tasks", (req, res) => {
   if (req.session.user.manager) {
@@ -86,12 +90,12 @@ app.get("/tasks", (req, res) => {
     res.render("./pages/employeeTasks", { auth: req.session.user });
   }
 });
-app.get("/managerTasks", (req, res) => {
-  res.render("./pages/managerTasks", { auth: req.session.user });
-});
-app.get("/employeeTasks", (req, res) => {
-  res.render("./pages/employeeTasks", { auth: req.session.user });
-});
+// app.get("/managerTasks", (req, res) => {
+//   res.render("./pages/managerTasks", { auth: req.session.user });
+// });
+// app.get("/employeeTasks", (req, res) => {
+//   res.render("./pages/employeeTasks", { auth: req.session.user });
+// });
 
 app.get("/register", (req, res) => {
   res.render("./pages/register", { auth: req.session.user });
@@ -138,13 +142,16 @@ app.post("/registerManager", async (req, res) => {
     console.log("successfully inserted into the database");
     res.redirect("/login");
   } catch (error) {
-    if (error.code === '23505')
-    {
-      res.render("pages/register_manager", { message: "username taken", error: true });
-    }
-    else
-    {
-      res.render("pages/registerManager", { message: "an error has occurred", error: true });
+    if (error.code === "23505") {
+      res.render("pages/register_manager", {
+        message: "username taken",
+        error: true,
+      });
+    } else {
+      res.render("pages/registerManager", {
+        message: "an error has occurred",
+        error: true,
+      });
     }
     console.log("Failed to register manager.");
     console.error("Error inserting into Database", error);
@@ -168,15 +175,17 @@ app.post("/registerEmployee", async (req, res) => {
     );
     console.log("successfully inserted into the database");
     res.redirect("/login");
-  } catch (error) 
-  {
-    if (error.code === '23505')
-    {
-      res.render("pages/registerEmployee", { message: "username taken", error: true });
-    }
-    else
-    {
-      res.render("pages/registerEmployee", { message: "an error has occurred", error: true });
+  } catch (error) {
+    if (error.code === "23505") {
+      res.render("pages/registerEmployee", {
+        message: "username taken",
+        error: true,
+      });
+    } else {
+      res.render("pages/registerEmployee", {
+        message: "an error has occurred",
+        error: true,
+      });
     }
     console.log("Failed to register employee.");
     console.error("Error inserting into Database", error);
@@ -199,14 +208,13 @@ app.post("/login", async (req, res) => {
       req.session.user = user;
       req.session.save();
       res.redirect("/employeeTasks");
+    } else {
+      return res.render("pages/login", {
+        message: "incorrect user or password",
+        error: true,
+      });
     }
-    else
-    {
-      return res.render("pages/login", {message: "incorrect user or password", error: true});
-    }
-  } 
-  catch (error) 
-  {
+  } catch (error) {
     console.log(error);
     res.render("pages/login", { message: "user not found", error: true });
   }
@@ -222,7 +230,6 @@ module.exports = app.listen(PORT, (error) => {
   else console.log("Error occurred, server can't start", error);
 });
 
-
-app.get('/welcome', (req, res) => {
-  res.json({status: 'success', message: 'Welcome!'});
+app.get("/welcome", (req, res) => {
+  res.json({ status: "success", message: "Welcome!" });
 });
--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -1,27 +1,29 @@
 // ********************** Initialize server **********************************
 
-const server = require('../index'); //TODO: Make sure the path to your index.js is correctly added
+const server = require("../index"); //TODO: Make sure the path to your index.js is correctly added
 
 // ********************** Import Libraries ***********************************
 
-const chai = require('chai'); // Chai HTTP provides an interface for live integration testing of the API's.
-const chaiHttp = require('chai-http');
+const chai = require("chai"); // Chai HTTP provides an interface for live integration testing of the API's.
+const chaiHttp = require("chai-http");
 chai.should();
 chai.use(chaiHttp);
-const {assert, expect} = chai;
+const { assert, expect } = chai;
 
 // ********************** DEFAULT WELCOME TESTCASE ****************************
 
-describe('Server!', () => {
+describe("Server!", () => {
   // Sample test case given to test / endpoint.
-  it('Returns the default welcome message', done => {
+  it("Returns the default welcome message", (done) => {
     chai
       .request(server)
-      .get('/welcome')
+      .get("/welcome")
       .end((err, res) => {
+        console.log("Test: " + res.body.status);
+        console.log(res.body);
         expect(res).to.have.status(200);
-        expect(res.body.status).to.equals('success');
-        assert.strictEqual(res.body.message, 'Welcome!');
+        expect(res.body.status).to.equals("success");
+        assert.strictEqual(res.body.message, "Welcome!");
         done();
       });
   });
