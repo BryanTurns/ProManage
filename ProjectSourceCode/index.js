3b624066c2bcd1160307bcd8654e0d3889062332
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2024-04-04 08:33:34 -0600

    Moved redirection from navbar to /tasks route in backend js

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -4,7 +4,7 @@ const Handlebars = require("handlebars");
 const path = require("path");
 const pgp = require("pg-promise")(); // To connect to the Postgres DB from the node server
 const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt'); //  To hash passwords
+const bcrypt = require("bcrypt"); //  To hash passwords
 const session = require("express-session"); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const axios = require("axios");
 
@@ -44,11 +44,13 @@ db.connect()
     console.log("ERROR:", error.message || error);
   });
 
-app.use(session({
-  secret: 'XASDASDA',
-  saveUninitialized: true,
-  resave: true
-}))
+app.use(
+  session({
+    secret: "XASDASDA",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -61,6 +63,17 @@ app.get("/", (req, res) => {
 app.get("/home", (req, res) => {
   res.redirect("/");
 });
+app.get("/tasks", (req, res) => {
+  if (req.session.user) {
+    if (req.session.user.manager) {
+      res.render("./pages/managerTasks");
+    } else {
+      res.render("./pages/employeeTasks");
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
 app.get("/managerTasks", (req, res) => {
   res.render("./pages/managerTasks");
 });
@@ -70,10 +83,6 @@ app.get("/employeeTasks", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("./pages/register");
-});
-
-app.get("/employeeTasks", (req, res) => {
-  res.render("./pages/employeeTasks");
 });
 
 app.get("/register_employee", (req, res) => {
@@ -88,97 +97,98 @@ app.get("/login", (req, res) => {
   res.render("./pages/login");
 });
 
-
-app.get('/logout',(req, res) =>
-{
-  res.render('pages/logout');
+app.get("/logout", (req, res) => {
+  res.render("pages/logout");
 });
 
-app.post('/logout',(req, res) =>
-{
-  req.session.destroy((err =>
-    {
-      if(err)
-      {
-        console.log("Error Logging Out");
-      };
-    }));
-    res.redirect('/login');
+app.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log("Error Logging Out");
+    }
+  });
+  res.redirect("/login");
 });
-app.post("/registerManager", async (req, res) => 
-{
-  try 
-  {
-    const hash = await bcrypt.hash(req.body.password, 10); 
-    await db.none("INSERT INTO users (username, password, firstname, lastname, branch, manager) VALUES ($1, $2, $3, $4, $5, $6)", [req.body.username, hash, req.body.firstname, req.body.lastname, req.body.branch, true]);
+app.post("/registerManager", async (req, res) => {
+  try {
+    const hash = await bcrypt.hash(req.body.password, 10);
+    await db.none(
+      "INSERT INTO users (username, password, firstname, lastname, branch, manager) VALUES ($1, $2, $3, $4, $5, $6)",
+      [
+        req.body.username,
+        hash,
+        req.body.firstname,
+        req.body.lastname,
+        req.body.branch,
+        true,
+      ]
+    );
     console.log("successfully inserted into the database");
     res.redirect("/login");
-  } 
-  catch (error) 
-  {
+  } catch (error) {
     console.log("Failed to register manager.");
-    console.error('Error inserting into Database', error);
+    console.error("Error inserting into Database", error);
     res.redirect("/register_manager");
   }
 });
 
-
-app.post("/registerEmployee", async (req, res) => 
-{
-  try 
-  {
+app.post("/registerEmployee", async (req, res) => {
+  try {
     const hash = await bcrypt.hash(req.body.password, 10); // Correctly wait for the hash to complete
-    await db.none("INSERT INTO users (username, password, firstname, lastname, branch, manager) VALUES ($1, $2, $3, $4, $5, $6)", [req.body.username, hash, req.body.firstname, req.body.lastname, req.body.branch, false]);
+    await db.none(
+      "INSERT INTO users (username, password, firstname, lastname, branch, manager) VALUES ($1, $2, $3, $4, $5, $6)",
+      [
+        req.body.username,
+        hash,
+        req.body.firstname,
+        req.body.lastname,
+        req.body.branch,
+        false,
+      ]
+    );
     console.log("successfully inserted into the database");
     res.redirect("/login");
-  } 
-  catch (error) 
-  {
+  } catch (error) {
     console.log("Failed to register employee.");
-    console.error('Error inserting into Database', error);
+    console.error("Error inserting into Database", error);
     res.redirect("/register_employee");
   }
 });
 
-app.post('/login', async(req, res) =>
-{
-  try
-  {
+app.post("/login", async (req, res) => {
+  try {
     console.log(req.body.username);
-    console.log('SELECT * FROM users WHERE username = $1;');
-    const user = await db.one('SELECT * FROM users WHERE username = $1;', [req.body.username]);
-    const match = await bcrypt.compare(req.body.password, user.password); 
-    if (match & req.body.manager) 
-    {
+    console.log("SELECT * FROM users WHERE username = $1;");
+    const user = await db.one("SELECT * FROM users WHERE username = $1;", [
+      req.body.username,
+    ]);
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (match & req.body.manager) {
       req.session.user = user;
       req.session.save();
-      res.redirect("/managerTasks"); 
+      res.redirect("/managerTasks");
+    } else if (match & !req.body.manager) {
+      req.session.user = user;
+      req.session.save();
+      res.redirect("/employeeTasks");
     }
-    else if(match & !req.body.manager)
-    {
-      req.session.user = user;
-      req.session.save();
-      res.redirect('/employeeTasks');
-    };
-     return res.render('pages/login', {message: 'incorrect user or password', error: true});
-  }
-  catch (error)
-  {
+    return res.render("pages/login", {
+      message: "incorrect user or password",
+      error: true,
+    });
+  } catch (error) {
     console.log(error);
-    res.render("pages/login", {message: 'user not found', error: true});
-  };
+    res.render("pages/login", { message: "user not found", error: true });
+  }
 });
 // Authentication Middleware.
-const auth = (req, res, next) => 
-{
+const auth = (req, res, next) => {
   if (!req.session.user) {
     // Default to login page.
-    return res.redirect('/login');
+    return res.redirect("/login");
   }
   next();
 };
-
-
 
 app.get("/login", (req, res) => {
   res.render("./pages/login");
